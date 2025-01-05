668ef7f248130c1a0c0b48b1ff55f1e9d96d99ab
sanchezhs <ssancheztoca@gmail.com>
2025-01-05 17:35:33 +0100

    Trying HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css'
 import CustomNavbar from './navbar/CustomNavbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter, BrowserRouter as Router } from 'react-router-dom';
 import AnimatedRoutes from './routes/AnimatedRoutes';
 import Footer from './footer/Footer';
 
 function App() {
   return (
     <>
-      <Router basename='/portfolio/'>
+      <HashRouter>
         <CustomNavbar />
         <div style={{ marginTop: '80px', padding: '20px' }}>
           <AnimatedRoutes />
         </div>
         <Footer />
-      </Router>
+      </HashRouter>
     </>
   );
 }
